cf0de71be827197d95afa1ba42f0ddd2dd78ae8b
Monish Saravana <monish.sd.2008@gmail.com>
2022-09-24 20:24:56 -0400

    Added the .exe for the Image Processing App
    Packed the Image Processing App as a an exe.

--- Sorbent Deployment/Arduino Code/Rotating Dispenser with Servo Motor/RotatingDispenserServov1.ino
+++ Sorbent Deployment/Arduino Code/Rotating Dispenser with Servo Motor/RotatingDispenserServov1.ino
@@ -2,49 +2,72 @@
 #include <Servo.h>
 Servo DispenserServo;
 
+String inputString = ""; 
 char input; 
 int pos = 0;
+int inputInt;
+int timeMillis;
 SoftwareSerial BT(3, 2);
 
 void setup() {
   DispenserServo.attach(9); 
-Serial.begin(9600); 
-BT.begin(9600); 
-Serial.println("The bluetooth gates are open.\n Connect to HC-05 from any other bluetooth device with 1234 as pairing key!.");
-//  DispenserServo.write(180);
-//  delay(1000); 
-//  DispenserServo.write(0);
-  }
+  Serial.begin(9600); 
+  BT.begin(9600); 
+  Serial.println("The bluetooth gates are open.\n Connect to HC-05 from any other bluetooth device with 1234 as pairing key!.");
+}
 
 void loop() { 
-
-   input = BT.read();  
-    
-    if (input == 'A'){
-//        pos = 0;
-//        Serial.println("It's A"); 
-//        Serial.println(pos); 
-//        DispenserServo.write(pos);
-//        delay(1500); 
-//        pos = 180; 
-//        Serial.println(pos); 
-//        DispenserServo.write(pos);
-//        delay(1500);
-      if (pos == 0) {
-        pos = 180; 
-//        delay(1500); 
-        Serial.println(pos); 
-        DispenserServo.write(pos);
-        delay(1500); 
-      }
-      else if (pos == 180) {
-        pos = 0; 
-//          delay(1500); 
-        Serial.println(pos); 
-        DispenserServo.write(pos);
-        delay(1500); 
-      }
+//    Serial.println(BT.available());    
+    while (BT.available()) {
+        delay(100);  //small delay to allow input buffer to fill
+        input = BT.read();   //gets one byte from serial buffer]
+        Serial.println("input read sucessfully" + input); 
+        if (isDigit(input)){
+                Serial.println("read the number: " + input);
+                inputString.concat(input);
     }
-  // put your main code here, to run repeatedly:
+        else {
+          Serial.println("The character is not a number" + input);
+             } 
+          }  
 
+    if (inputString.length() > 0) {
+
+      if (inputString == 0){
+        pos = 0; 
+        DispenserServo.write(pos);
+        timeMillis = 0; 
+      }
+      else { 
+      
+       Serial.println("the time is " + inputString); //prints string to serial port out
+    
+        inputInt = inputString.toInt();
+        inputString="";
+        Serial.println("the num of seconds to turn is: " + inputString); 
+
+     if (inputInt > 0){
+    
+      turnServo(inputInt, DispenserServo);   
+    }
+    }
+    }
+    }
+    
+   
+
+void  turnServo (int timeSec, Servo servo) {
+    timeMillis = (timeSec * 1000); 
+    Serial.println("the time in millis is " + String(timeMillis)); 
+    pos = 180; 
+    servo.write(pos);
+    delay (timeMillis); 
+    returnServo(servo); 
+  
+}
+
+void returnServo (Servo servo){
+  servo.write(0); 
+  pos = 0;
+  delay(1000);  
 }
