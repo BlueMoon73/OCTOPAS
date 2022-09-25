10602aa0d195b00b9069cd5903d5b6b50eaa2b49
Monish Saravana <monish.sd.2008@gmail.com>
2022-09-25 18:33:24 -0400

    Updated picking images
    Reformatting, and updating the picking images feature. No need to clear the image before picking.

--- Image Processing/App/Windows/main.py
+++ Image Processing/App/Windows/main.py
@@ -1,36 +1,29 @@
 import os
+import time
+
 import kivy
 from kivy import platform
 
 kivy.require('2.0.0')
-from kivy.uix.image import Image
 
-import math
 from kivy.config import Config
 
 Config.set('input', 'mouse', 'mouse,multitouch_on_demand')
 import cv2 as cv
-from time import process_time
 import numpy as np
 from kivy.app import App
-from kivy.uix.label import Label
 from kivy.uix.screenmanager import ScreenManager, Screen
 from matplotlib import pyplot as plt
 from sklearn import cluster
-from kivy.core.text import LabelBase
 from kivy.core.window import Window
 
 Window.clearcolor = (1, 1, 1, 1)
 from kivy.utils import get_color_from_hex as rgba
-import kivy.core.text.markup
 
 if platform == "android":
     from android.permissions import request_permissions, Permission
 
     request_permissions([Permission.READ_EXTERNAL_STORAGE, Permission.WRITE_EXTERNAL_STORAGE])
-
-
-# LabelBase.register(name='Gill', fn_regular="Gill Sans MT.ttf")
 
 
 class oilSpillImage:
@@ -47,7 +40,9 @@ class oilSpillImage:
         return cv.resize(img, dims, interpolation=cv.INTER_AREA)
 
     def findOil(self, img, number=2):
-        startTime = process_time()
+        startTime = time.perf_counter()
+        # print(startTime)
+        print("xxxxxxxxxxxx")
         img = cv.cvtColor(img, cv.COLOR_BGR2HSV) / 255
         # set number of colors
 
@@ -74,7 +69,7 @@ class oilSpillImage:
         # get the unique colors
         colors, counts = np.unique(img4, return_counts=True, axis=0)
         # print(colors)
-        print("xxx")
+        # print("xxx")
         # print(counts)
         unique = zip(colors, counts)
 
@@ -99,7 +94,8 @@ class oilSpillImage:
         # fig.savefig('barn_color_historgram.png')
         # plt.close(fig)
 
-        endTime = process_time()
+        endTime = time.perf_counter()
+        # print(endTime)
 
         processingTime = endTime - startTime
 
@@ -112,18 +108,19 @@ class oilSpillImage:
         print(str(self.scaleH), str(self.scaleW))
         self.combinedScale = (self.scaleW + self.scaleW) / 2
         totalWaterArea = Hcm * Wcm
+        # cv.imshow('final', img3)
 
         return img3, processingTime, totalWaterArea, self.scaleH, self.scaleW, self.combinedScale
 
-    def makePerimeters(self, real, reduced, areaReq=1000):
+    def makePerimeters(self, real, reduced, areaReq=3000):
         reduced = cv.cvtColor(reduced, cv.COLOR_BGR2GRAY)
-        # cv.imshow('graty', reduced)
+        cv.imshow('graty', reduced)
 
         ###CODE  THAT WORKS###
         # (thresh, reduced) = cv.threshold(reduced, 90, 255, cv.THRESH_BINARY)
         reduced = cv.adaptiveThreshold(reduced, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY, 3, 0)
-
-        contours, hierarchies = cv.findContours(reduced, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+        # cv.imshow('thresh', reduced)
+        contours, hierarchies = cv.findContours(reduced, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
         # cv.imshow('v2',reduced)
 
         final = real;
@@ -149,10 +146,10 @@ class oilSpillImage:
                 oil_contours.append(contours[i])
 
                 epsilon = 0.0008 * cv.arcLength(contours[i], True)
-                poly = cv.approxPolyDP(contours[i], epsilon, True);
-                final = cv.drawContours(final, [poly], -1, (255, 200, 0), -1);
+                poly = cv.approxPolyDP(contours[i], epsilon, True)
+                # final = cv.drawContours(final, [contours], -1, (255, 200, 0), -1)
 
-                # final = cv.drawContours(final, contours, i, (255, 0, 0), -1)
+                final = cv.drawContours(final, poly, -1, (255, 0, 0), -1)
 
                 kernel = np.ones((9, 9), np.uint8)
                 final = cv.morphologyEx(final, cv.MORPH_CLOSE, kernel)
@@ -193,53 +190,53 @@ class oilSpillImage:
         hullLengthinCM = hullLengthinPX / self.combinedScale
         return final, contours, totalOilArea, hullLengthinCM
 
-    def label(self, finalimg, ellipse, totalOilArea, circlerad, largestContourArea, hull, px, cm):
-        scale = px / cm
-
-        # ellipse perimeter
-        (x, y), (width, height), angle = ellipse
-        a = width / 2
-        a = a / scale
-        b = width / 2
-        b = b / scale
-        h = (a - b) ** 2 / (a + b) ** 2
-
-        # ellipse boom
-        perimeterOfEllipseBoominPX = (math.pi * (a + b) * (1 + (3 * h) / (10 + np.sqrt((4 - (3 * h))))))
-        perimeterOfEllipseBoominCM = perimeterOfEllipseBoominPX
-
-        # circle boom
-        perimeterOfCircleBoominPX = (2 * math.pi * circlerad)
-        perimeterOfCircleBoominCM = (perimeterOfCircleBoominPX / scale);
-
-        # oil area
-        totalOilAreainCM2 = totalOilArea / (scale * scale)
-        largestContourAreainCM = largestContourArea / (scale * scale)
-
-        # hull length
-        hullLengthinPX = cv.arcLength(hull, closed=True)
-        hullLengthinCM = hullLengthinPX / scale
-
-        # percent of oil
-        containerArea = cm ** 2 * math.pi
-        percentOfOil = totalOilAreainCM2 * 100 / containerArea
-
-        # labeling
-        cv.putText(finalimg, ('Total Oil Area: ' + str((totalOilAreainCM2)) + ' cm'), (0, 15), cv.FONT_HERSHEY_SIMPLEX,
-                   0.5,
-                   (255, 0, 0), 2)
-        cv.putText(finalimg, ('Largest Patch Area: ' + str((largestContourAreainCM)) + ' cm'), (0, 30),
-                   cv.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2)
-        cv.putText(finalimg, ('Length of Ellipse Boom: ' + str((perimeterOfEllipseBoominCM)) + ' cm'), (0, 45),
-                   cv.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 128), 2)
-        cv.putText(finalimg, ('Length of Circle Boom: ' + str((perimeterOfCircleBoominCM)) + ' cm'), (0, 60),
-                   cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 255), 2)
-        cv.putText(finalimg, ('Length of Hull Boom: ' + str((hullLengthinCM)) + ' cm'), (0, 75),
-                   cv.FONT_HERSHEY_SIMPLEX,
-                   0.5, (165, 255, 0), 2)
-        cv.putText(finalimg, ('Percent of Oil in Container: ' + str((percentOfOil)) + ' %'), (0, 90),
-                   cv.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
-        return finalimg
+    # def label(self, finalimg, ellipse, totalOilArea, circlerad, largestContourArea, hull, px, cm):
+    #     scale = px / cm
+    #
+    #     # ellipse perimeter
+    #     (x, y), (width, height), angle = ellipse
+    #     a = width / 2
+    #     a = a / scale
+    #     b = width / 2
+    #     b = b / scale
+    #     h = (a - b) ** 2 / (a + b) ** 2
+    #
+    #     # ellipse boom
+    #     perimeterOfEllipseBoominPX = (math.pi * (a + b) * (1 + (3 * h) / (10 + np.sqrt((4 - (3 * h))))))
+    #     perimeterOfEllipseBoominCM = perimeterOfEllipseBoominPX
+    #
+    #     # circle boom
+    #     perimeterOfCircleBoominPX = (2 * math.pi * circlerad)
+    #     perimeterOfCircleBoominCM = (perimeterOfCircleBoominPX / scale);
+    #
+    #     # oil area
+    #     totalOilAreainCM2 = totalOilArea / (scale * scale)
+    #     largestContourAreainCM = largestContourArea / (scale * scale)
+    #
+    #     # hull length
+    #     hullLengthinPX = cv.arcLength(hull, closed=True)
+    #     hullLengthinCM = hullLengthinPX / scale
+    #
+    #     # percent of oil
+    #     containerArea = cm ** 2 * math.pi
+    #     percentOfOil = totalOilAreainCM2 * 100 / containerArea
+    #
+    #     # labeling
+    #     cv.putText(finalimg, ('Total Oil Area: ' + str((totalOilAreainCM2)) + ' cm'), (0, 15), cv.FONT_HERSHEY_SIMPLEX,
+    #                0.5,
+    #                (255, 0, 0), 2)
+    #     cv.putText(finalimg, ('Largest Patch Area: ' + str((largestContourAreainCM)) + ' cm'), (0, 30),
+    #                cv.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2)
+    #     cv.putText(finalimg, ('Length of Ellipse Boom: ' + str((perimeterOfEllipseBoominCM)) + ' cm'), (0, 45),
+    #                cv.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 128), 2)
+    #     cv.putText(finalimg, ('Length of Circle Boom: ' + str((perimeterOfCircleBoominCM)) + ' cm'), (0, 60),
+    #                cv.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 255), 2)
+    #     cv.putText(finalimg, ('Length of Hull Boom: ' + str((hullLengthinCM)) + ' cm'), (0, 75),
+    #                cv.FONT_HERSHEY_SIMPLEX,
+    #                0.5, (165, 255, 0), 2)
+    #     cv.putText(finalimg, ('Percent of Oil in Container: ' + str((percentOfOil)) + ' %'), (0, 90),
+    #                cv.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 2)
+    #     return finalimg
 
     # def analyze(self, processedIMG):
 
@@ -256,15 +253,15 @@ class FilePickerScreen(Screen):
         print(len(self.children))
 
     def _finish_init(self):
-        self.layout = self.ids["displayOverlay"]
-        self.OilImage = self.ids["OilImage"]
-        self.OilImage.nocache = True
-        # self.OilImage.size = self.parent.width, self.parent.width/self.image_ratio
+        # self.layout = self.ids["displayOverlay"]
+        self.OilPic = self.ids["OilImage"]
+        self.OilPic.nocache = True
+        # self.OilPic.size = self.parent.width, self.parent.width/self.image_ratio
 
         self.filePicker = self.ids["filechooser"]
         self.filePicker.bind(on_entry_added=self.update_file_list_entry)
         self.filePicker.bind(on_subentry_to_entry=self.update_file_list_entry)
-        # self.layout.add_widget(self.OilImage)
+        # self.layout.add_widget(self.OilPic)
         self.title = self.ids["title"]
         self.processTimeLabel = self.ids["processTime"]
         self.areaOfWaterLabel = self.ids["areaOfWater"]
@@ -279,9 +276,19 @@ class FilePickerScreen(Screen):
         self.fileName = filename
         # print("selected: %s" % filename[0])
 
+    def init_widget(self, *args):
+
+        fc = self.ids['filechooser']
+        fc.bind(on_entry_added=self.update_file_list_entry)
+        fc.bind(on_subentry_to_entry=self.update_file_list_entry)
+
     def update_file_list_entry(self, file_chooser, file_list_entry, *args):
-        file_list_entry.font_size = 30
+        file_list_entry.font_size = 20
+        # file_list_entry.
         file_list_entry.ids['filename'].color = rgba("#005c7a")
+        # file_list_entry.children[0].color = rgba("#005c7a")  # File Names
+        # file_list_entry.children[1].color = rgba("#005c7a")
+        # file_list_entry.ids['filesize'].color = rgba("#005c7a")
 
     def open(self, path, fileName):
 
@@ -289,17 +296,17 @@ class FilePickerScreen(Screen):
             file = os.path.join(path, fileName[0])
             pic = cv.imread(file)
             (h, w) = pic.shape[:2]
+            print("trying to open")
+            print(file)
             if h > w:
-
                 pic = cv.rotate(pic, cv.ROTATE_90_CLOCKWISE)
                 cv.imwrite('originalImage.png', pic)
                 print('rotated')
                 print(h, w)
-
-            else:
-                cv.imwrite('originalImage.png', pic)
-
-            self.OilImage.source = 'originalImage.png'
+                # self.OilPic.reload()
+            cv.imwrite('originalImage.png', pic)
+            self.OilPic.source = ""
+            self.OilPic.source = 'originalImage.png'
 
         except IndexError:
             self.title.text = "Please select an Image!"
@@ -314,45 +321,54 @@ class FilePickerScreen(Screen):
 
     def process(self):
 
-        file = self.OilImage.source
+        file = self.OilPic.source
         if (file != "PleasePickAnImage.png"):
+            StartTime = time.perf_counter()
+
             pic = cv.imread(file)
             self.processPic = oilSpillImage(pic)
             print("file being processed" + file)
 
-            reducedColors, processingTime, totalWaterAreainCM3, self.scaleH, self.scaleW, self.combinedScale = self.processPic.findOil(pic)
+            reducedColors, processingTime, totalWaterAreainCM2, self.scaleH, self.scaleW, self.combinedScale = self.processPic.findOil(
+                pic)
 
-            final, contours, totalOilAreainPx, hullLengthinCM3 = self.processPic.makePerimeters(pic, reducedColors, areaReq=100)
+            final, contours, totalOilAreainPx, hullLengthinCM3 = self.processPic.makePerimeters(pic, reducedColors,
+                                                                                                areaReq=1000)
             cv.imwrite('processed_pic.jpg', final)
+            # cv.imshow('processed_pic.jpg', final)
+            totalOilAreainCM2 = totalOilAreainPx / self.scaleH
+            totalOilAreainCM2 = totalOilAreainCM2 / self.scaleW
+            totalOilAreainCM2 = round(totalOilAreainCM2, 2)
 
-            totalOilAreainCM3 = totalOilAreainPx / self.scaleH
-            totalOilAreainCM3 = totalOilAreainCM3 / self.scaleW
-            totalOilAreainCM3 = round(totalOilAreainCM3, 2)
-
-            totalOilPercent = (totalOilAreainCM3 / totalWaterAreainCM3) * 100
+            totalOilPercent = (totalOilAreainCM2 / totalWaterAreainCM2) * 100
             totalOilPercent = round(totalOilPercent, 2)
 
             hullLengthinCM3 = round(hullLengthinCM3, 2)
             # self.combinedScale = (self.scaleW + self.scaleH) / 2
+            # print(processingTime)
 
-
-
-            self.OilImage.source = 'processed_pic.jpg'
-            self.OilImage.reload()
+            # processingTime = round(processingTime, 2)
+            print(processingTime)
+            self.OilPic.source = 'processed_pic.jpg'
+            self.OilPic.reload()
 
             self.title.text = "Image Analytics:  "
             self.processTimeLabel.text = "The time taken to process the image was: " + str(processingTime) + " seconds!"
-            self.areaOfWaterLabel.text = "The area of water is " + str(totalWaterAreainCM3) + " cubic centimeters"
-            self.areaOfOilLabel.text = "The area of oil is " + str(totalOilAreainCM3) + " cubic centimeters"
+            self.areaOfWaterLabel.text = "The area of water is " + str(totalWaterAreainCM2) + " square centimeters"
+            self.areaOfOilLabel.text = "The area of oil is " + str(totalOilAreainCM2) + " square centimeters"
             self.percentOfOilLabel.text = "The percent of oil in the water is " + str(totalOilPercent) + "%"
-            self.lengthOfBoomLabel.text = "The length of the boom is " + str(hullLengthinCM3) +  " centimeters"
+            self.lengthOfBoomLabel.text = "The length of the boom is " + str(hullLengthinCM3) + " centimeters"
+            endTime = time.perf_counter()
+            processingTime = endTime - StartTime
+            processingTime = round(processingTime, 2)
+            self.processTimeLabel.text = "The time taken to process the image was: " + str(processingTime) + " seconds!"
         else:
             self.title.text = "Please select an Image!"
 
     def clear(self, path):
-        print("cleared!")
+        # print("cleared!")
         self.title.text = "Image Succesfully Cleared!"
-        self.OilImage.source = 'PleasePickAnImage.png'
+        self.OilPic.source = 'PleasePickAnImage.png'
         self.clearText()
 
     def clearText(self):
@@ -371,7 +387,6 @@ class WindowManager(ScreenManager):
     pass
 
 
-# kv = Builder.load_file('octopas.kv')
 class OctopasAPP(App):
 
     def build(self):
@@ -379,8 +394,6 @@ class OctopasAPP(App):
         aids = FilePickerScreen(name='first')
         aids._finish_init()
         sm.add_widget(aids)
-
-        # sm.add_widget(FP)
         return sm
 
 
